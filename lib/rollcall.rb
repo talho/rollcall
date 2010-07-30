347820d01f7473085afadfe9b6bc8d885347c53d
cacornell <cacornell@gmail.com>
2010-07-30 10:15:31 -0500

    Added open_flash_chart as a submodule in vendor/plugins.

--- lib/rollcall.rb
+++ lib/rollcall.rb
@@ -1,2 +1,10 @@
 require File.join(File.dirname(__FILE__), 'models', 'user.rb')
 require File.join(File.dirname(__FILE__), 'models', 'jurisdiction.rb')
+
+# Add rollcall vendor/plugins/*/lib to LOAD_PATH
+Dir[File.join(File.dirname(__FILE__), '../vendor/plugins/*/lib')].each do |path|
+  $LOAD_PATH << path
+end
+
+# Require the open_flash_chart init.rb
+require File.join(File.dirname(__FILE__), '..', 'vendor', 'plugins', 'open_flash_chart', 'init.rb')

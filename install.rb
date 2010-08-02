dbc2eb4ef14cb0c646a86ae71989d5eeca9a5d04
cacornell <cacornell@gmail.com>
2010-08-02 15:40:15 -0500

    Added comments.

--- install.rb
+++ install.rb
@@ -1,6 +1,8 @@
 # Install hook code here
 FileUtils.cp_r(File.join(File.dirname(__FILE__),'lib','workers/.'),File.join(Rails.root,'lib','workers'))
 
+# Create links in Rails.root/public so that the register_javascript_expansion()
+# and register_stylesheet_expansion() methods can see the plugin's files.
 parent_public_dir = File.join(Rails.root, "public")
 [ "javascripts", "stylesheets" ].each { |public_subdir|
   rel_path = File.join("..","..","vendor","plugins","rollcall","public",public_subdir)

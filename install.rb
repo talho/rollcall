686272ddcc67cb8e86f739c2d48ffa8ee8c2497c
cacornell <cacornell@gmail.com>
2010-08-03 14:20:53 -0500

    A few changes to match the openphin_plugin generator actions.

--- install.rb
+++ install.rb
@@ -2,13 +2,19 @@
 parent_lib_dir = File.join(Rails.root, "lib")
 [ "workers" ].each { |lib_subdir|
   rel_path = File.join("..","..","vendor","plugins","rollcall","lib",lib_subdir)
-  File.symlink(rel_path, File.join(parent_lib_dir, lib_subdir, "rollcall"))
+  target = File.join(parent_lib_dir, lib_subdir, "rollcall")
+  File.symlink(rel_path, target) unless File.exists?(target)
 }
+
+rel_path = File.join("..","..","..","..","spec","spec_helper.rb")
+target = "#{Rails.root}/vendor/plugins/rollcall/spec/spec_helper.rb"
+File.symlink(rel_path, target) unless File.exists?(target)
 
 # Create links in Rails.root/public so that the register_javascript_expansion()
 # and register_stylesheet_expansion() methods can see the plugin's files.
 parent_public_dir = File.join(Rails.root, "public")
 [ "javascripts", "stylesheets" ].each { |public_subdir|
   rel_path = File.join("..","..","vendor","plugins","rollcall","public",public_subdir)
-  File.symlink(rel_path, File.join(parent_public_dir, public_subdir, "rollcall"))
+  target = File.join(parent_public_dir, public_subdir, "rollcall")
+  File.symlink(rel_path, target) unless File.exists?(target)
 }
--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,14 @@
-# Uninstall hook code here
+parent_lib_dir = File.join(Rails.root, "lib")
+[ "workers" ].each { |lib_subdir|
+  target = File.join(parent_lib_dir, lib_subdir, "rollcall")
+  File.unlink(target) if File.exists?(target)
+}
+
+target = "#{Rails.root}/vendor/plugins/rollcall/spec/spec_helper.rb"
+File.unlink(target) if File.exists?(target)
+
+parent_public_dir = File.join(Rails.root, "public")
+[ "javascripts", "stylesheets" ].each { |public_subdir|
+  target = File.join(parent_public_dir, public_subdir, "rollcall")
+  File.unlink(target) if File.exists?(target)
+}

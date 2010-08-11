5df47be240f42ec4d60063eee7d0e2fa80954f4d
cacornell <cacornell@gmail.com>
2010-08-11 16:49:08 -0500

    converted cucmber tests to capybara

--- features/step_definitions/absentee_reports_steps.rb
+++ features/step_definitions/absentee_reports_steps.rb
@@ -1,5 +1,6 @@
 Then /^I should see an absenteeism graph with the data\:$/ do |table|
-  response.should have_selector("img[src*=\"chd=t:#{table.raw.map(&:first).join(",")}\"]")
+  #response.should have_selector("img[src*=\"chd=t:#{table.raw.map(&:first).join(",")}\"]")
+  assert find(:css, "img[src*=\"chd=t:#{table.raw.map(&:first).join(",")}\"]")
 end
 
 
@@ -10,32 +11,32 @@ Then /^I should see an absenteeism summary with the data\:$/ do |table|
   end
 end
 Then /^I should see an absenteeism graph with the following\:$/ do |table|
-  raise "No data URL located in response body" unless response.body =~ /\{"data-file\"\:\"([^\"]*)\"/x
-  url = $1.gsub(/%2F/, "/")
+  raise "No data URL located in response body" unless page.body =~ /"data-file":"([^\"]*)"/
+  url = CGI.unescape($1)
   values = table.is_a?(Array) ? table : table.raw
   
-  in_a_separate_session do
-    visit url
-    json = ActiveSupport::JSON.decode(response.body)
-    values.each do |row|
-      property, value=row
-      case property
-        when /^data$/
-          data_values = value.split(",").map{|v| v=="nil" ? nil : v.to_f}
-          json['elements'].detect{|elm| elm["values"].map{|e| e.is_a?(Hash) ? e['value'] : e} == data_values }.should_not be_nil
-        when /^data-label$/
-          json['elements'].detect{|elm| elm["text"]==value}.should_not be_nil
-        when /^title$/
-          json['title']['text'].should == value
-        when /^range$/
-          min, max = value.split(',').map(&:to_f)
-          json['y_axis']['min'].should == min
-          json['y_axis']['max'].should == max
-      end
+  old_url = page.current_url
+  page.visit url
+  json = ActiveSupport::JSON.decode(page.find("body").text)
+  values.each do |row|
+    property, value=row
+    case property
+      when /^data$/
+        data_values = value.split(",").map{|v| v=="nil" ? nil : v.to_f}
+        json['elements'].detect{|elm| elm["values"].map{|e| e.is_a?(Hash) ? e['value'] : e} == data_values }.should_not be_nil
+      when /^data-label$/
+        json['elements'].detect{|elm| elm["text"]==value}.should_not be_nil
+      when /^title$/
+        json['title']['text'].should == value
+      when /^range$/
+        min, max = value.split(',').map(&:to_f)
+        json['y_axis']['min'].should == min
+        json['y_axis']['max'].should == max
     end
   end
-
+  page.visit old_url
 end
+
 Then /^I should see the (\d+)-day chart for "([^\"]*)" with the data:$/ do |timespan, schoolname, table|
   Then(%Q{the data at #{school_chart_path(School.find_by_display_name(schoolname), timespan)} should contain}, table)
 end

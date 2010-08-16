2cf3c00a06c8c4b1110846d0837bb6ce8d028a87
cacornell <cacornell@gmail.com>
2010-08-16 09:25:56 -0500

    Corrected test for existence of symbolic links.

--- uninstall.rb
+++ uninstall.rb
@@ -1,14 +1,14 @@
 parent_lib_dir = File.join(Rails.root, "lib")
 [ "workers" ].each { |lib_subdir|
   target = File.join(parent_lib_dir, lib_subdir, "rollcall")
-  File.unlink(target) if File.exists?(target)
+  File.unlink(target) if File.symlink?(target)
 }
 
 target = "#{Rails.root}/vendor/plugins/rollcall/spec/spec_helper.rb"
-File.unlink(target) if File.exists?(target)
+File.unlink(target) if File.symlink?(target)
 
 parent_public_dir = File.join(Rails.root, "public")
 [ "javascripts", "stylesheets" ].each { |public_subdir|
   target = File.join(parent_public_dir, public_subdir, "rollcall")
-  File.unlink(target) if File.exists?(target)
+  File.unlink(target) if File.symlink?(target)
 }

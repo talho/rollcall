b4729aef75cab38a527da1c59de03e4b0751218e
cacornell <cacornell@gmail.com>
2010-08-16 09:25:56 -0500

    Corrected test for existence of symbolic links.

--- install.rb
+++ install.rb
@@ -3,12 +3,12 @@ parent_lib_dir = File.join(Rails.root, "lib")
 [ "workers" ].each { |lib_subdir|
   rel_path = File.join("..","..","vendor","plugins","rollcall","lib",lib_subdir)
   target = File.join(parent_lib_dir, lib_subdir, "rollcall")
-  File.symlink(rel_path, target) unless File.exists?(target)
+  File.symlink(rel_path, target) unless File.symlink?(target)
 }
 
 rel_path = File.join("..","..","..","..","spec","spec_helper.rb")
 target = "#{Rails.root}/vendor/plugins/rollcall/spec/spec_helper.rb"
-File.symlink(rel_path, target) unless File.exists?(target)
+File.symlink(rel_path, target) unless File.symlink?(target)
 
 # Create links in Rails.root/public so that the register_javascript_expansion()
 # and register_stylesheet_expansion() methods can see the plugin's files.
@@ -16,5 +16,5 @@ parent_public_dir = File.join(Rails.root, "public")
 [ "javascripts", "stylesheets" ].each { |public_subdir|
   rel_path = File.join("..","..","vendor","plugins","rollcall","public",public_subdir)
   target = File.join(parent_public_dir, public_subdir, "rollcall")
-  File.symlink(rel_path, target) unless File.exists?(target)
+  File.symlink(rel_path, target) unless File.symlink?(target)
 }

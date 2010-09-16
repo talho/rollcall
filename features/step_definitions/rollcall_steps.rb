c65e098d8d15646ad8f864efa780fba0ea71d588
cacornell <cacornell@gmail.com>
2010-09-16 14:15:01 -0500

    Added ext_home feature file for ext testing.

--- features/step_definitions/rollcall_steps.rb
+++ features/step_definitions/rollcall_steps.rb
@@ -1,3 +1,7 @@
+When /^I set default_wait_time to (\d+)/ do |wait_time|
+  Capybara.default_wait_time = wait_time.to_i
+end
+
 When /^I drop the following file in the rollcall directory\:$/ do |erb_file_template|
   rollcall_drop_dir=File.join(File.dirname(__FILE__), '..', '..', 'tmp', 'rollcall')
   Dir.ensure_exists(rollcall_drop_dir)
@@ -15,10 +19,10 @@ Then /^I should not see a rollcall alert for "([^\"]*)"$/ do |school|
 end
 
 Then /^I should see an "([^\"]*)" rollcall summary for "([^\"]*)" with (.*) absenteeism$/ do |severity, school, percent|
-  school_node = find(:css, ".rollcall_summary .school", :content => school)
-  assert school_node.find(:css, ".#{severity} .absence").text == percent
+  school_node = find(:css, ".rollcall_summary .school", :text => school)
+  assert school_node.find(:css, "li.#{severity} span.absence").text == percent
 end
 
 Then /^I should see school data for "([^\"]*)"$/ do |school|
-  assert find(:css, ".school_data .school", :content => school) != nil
+  assert find(:css, ".school_data .school", :text => school) != nil
 end

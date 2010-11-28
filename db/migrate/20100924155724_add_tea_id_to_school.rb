12f8682738e631136d11f4c40263023f5d76406b
Ethan Waldo <ewaldo@talho.org>
2010-11-28 16:45:58 -0600

    Somehow forgot the school_id column needed for seeded data

--- db/migrate/20100924155724_add_tea_id_to_school.rb
+++ db/migrate/20100924155724_add_tea_id_to_school.rb
@@ -1,11 +1,15 @@
 class AddTeaIdToSchool < ActiveRecord::Migration
   def self.up
     add_column :schools, :tea_id, :integer
+    add_column :schools, :school_id, :integer
     add_index :schools, :tea_id
+    add_index :schools, :school_id
   end
 
   def self.down
-    remove_column :schools, :tea_id
+    remove_index :schools, :school_id
     remove_index :schools, :tea_id
+    remove_column :schools, :school_id
+    remove_column :schools, :tea_id
   end
 end

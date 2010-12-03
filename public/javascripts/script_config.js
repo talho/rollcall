72c1c6279fdedf01e6d740a172fd67c2ea8551f1
Ethan Waldo <ewaldo@talho.org>
2010-12-03 11:19:58 -0600

    Moved ComboBox to new ux location

--- public/javascripts/script_config.js
+++ public/javascripts/script_config.js
@@ -1,7 +1,8 @@
 Talho.ScriptManager.addInitializer('Talho.Rollcall', {js:'AjaxPanel /javascripts/rollcall/rollcall.js'});
 Talho.ScriptManager.addInitializer('Talho.Rollcall.NurseAssistant', {js:'$(ext_extensions)/xActionColumn.js /javascripts/rollcall/NurseAssistant.js'});
 Talho.ScriptManager.addInitializer('Talho.RollcallQuery', {
-  js:'( $(ext_extensions)/Portal.js > /javascripts/rollcall/SavedQueriesPanel.js ) $(ext_extensions)/HBox.js ' +
+  js:'( $(ext_extensions)/Portal.js > /javascripts/rollcall/ux/ComboBox.js > ' +
+     '/javascripts/rollcall/SavedQueriesPanel.js ) $(ext_extensions)/HBox.js ' +
      '( /javascripts/rollcall/MainPanel.js >  /javascripts/rollcall/SimpleSearchContainer.js ) '+
      ' /javascripts/rollcall/ReportsPanel.js ' +
      '/javascripts/rollcall/AlarmsPanel.js /javascripts/rollcall/AdvancedSearchContainer.js ' +

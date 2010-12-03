625e00f77dd438e97e6b24699a14880444b9fc3e
Ethan Waldo <ewaldo@talho.org>
2010-12-03 12:52:13 -0600

    Reworked the ADST form panel to a seperate class to move the listener from the ancestor object to the object itself and reduce Ext.getComponent usage.

--- public/javascripts/ADST.js
+++ public/javascripts/ADST.js
@@ -97,63 +97,94 @@ Talho.Rollcall.result_store = new Ext.data.JsonStore({
   }
 });
 
-Talho.RollcallQuery = Ext.extend(Ext.util.Observable, {
+Talho.Rollcall.ADSTFormPanel = Ext.extend(Ext.form.FormPanel, {
+  constructor: function(config) {
+    Ext.apply(config, {
+      itemId: 'ADSTFormPanel',
+      columnWidth: 1,
+      labelAlign: 'top',
+      id: "ADSTFormPanel",
+      url:'/rollcall/adst',
+      buttonAlign: 'left',
+      buttons: [{
+        text: "Submit",
+        scope: this,
+        handler: function(buttonEl, eventObj){
+          Ext.getCmp('ADSTFormPanel').getForm().submit({
+            scope: this,
+            waitMsg: "Please wait...",
+            waitTitle: "Loading",
+            success: function(form, action)
+            {
+              Ext.getCmp('ADSTResultPanel').show();
+              Ext.getCmp('ADSTResultPanel').processQuery(action.result);
+            },
+            failure: function(form, action)
+            {
+
+            }
+          });
+        },
+        formBind: true
+      },{
+        text: "Cancel",
+        handler: this.clearForm
+      }]
+    });
+
+    Talho.Rollcall.init_store = new Ext.data.JsonStore({
+      autoLoad: true,
+      root:   'options',
+      fields: ['absenteeism', 'age', 'data_functions', 'gender', 'grade', 'school_type',   'schools', 'symptons', 'temperature', 'zipcode'],
+      url:    '/rollcall/query_options',
+      listeners:{
+        scope: this,
+        load: function(this_store, record){
+          var simple_config = {};
+          var adv_config = {};
+          for(var i =0; i < record.length; i++){
+            if(record[i].data.schools != ""){
+              simple_config.schools = adv_config.schools = new Array();
+              for(var s = 0; s < record[i].data.schools.length; s++){
+                simple_config.schools[s] = adv_config.schools[s] = [
+                  record[i].data.schools[s].id, record[i].data.schools[s].display_name
+                ]
+              }
+            }else{
+              string_eval = "simple_config."+this_store.fields.items[i].name+" = adv_config."+this_store.fields.items[i].name+" = new Array();"+
+              "for(var a = 0; a "+"<"+" record[i].data."+this_store.fields.items[i].name+".length; a++){"+
+                  "simple_config."+this_store.fields.items[i].name+"[a] = adv_config."+this_store.fields.items[i].name+"[a] = ["+
+                    "record[i].data."+this_store.fields.items[i].name+"[a].id, record[i].data."+this_store.fields.items[i].name+"[a].value"+
+                  "];"+
+               "}"
+              eval(string_eval);
+            }
+          }
+          this.findParentByType('container').show();
+          this.add(new Talho.Rollcall.SimpleADSTContainer(simple_config));
+          this.add(new Talho.Rollcall.AdvancedADSTContainer(adv_config));
+          this.doLayout();
+        }
+      }
+    });
+
+    Talho.Rollcall.ADSTFormPanel.superclass.constructor.call(this, config);
+  }
+});
+
+Talho.Rollcall.ADST = Ext.extend(Ext.Panel, {
   constructor: function(config)
   {
-    Ext.apply(this, config);
-    Talho.RollcallQuery.superclass.constructor.call(this, config);
-    var panel = new Ext.Panel({
+    Ext.apply(config, {
       title:      config.title,
       id:         config.id,
       itemdId:    config.id,
       closable:   true,
       autoScroll: true,
       layout:     'border',
-      listeners: {
-        scope: this,
-        'render': function(this_panel){
-          Talho.Rollcall.init_store = new Ext.data.JsonStore({
-            autoLoad: true,
-            root:   'options',
-            fields: ['absenteeism', 'age', 'data_functions', 'gender', 'grade', 'school_type',   'schools', 'symptons', 'temperature', 'zipcode'],
-            url:    '/rollcall/query_options',
-            listeners:{
-              scope: this,
-              'load': function(this_store, record){
-                var simple_config = {};
-                var adv_config = {};
-                for(var i =0; i < record.length; i++){
-                  if(record[i].data.schools != ""){
-                    simple_config.schools = adv_config.schools = new Array();
-                    for(var s = 0; s < record[i].data.schools.length; s++){
-                      simple_config.schools[s] = adv_config.schools[s] = [
-                        record[i].data.schools[s].id, record[i].data.schools[s].display_name
-                      ]
-                    }
-                  }else{
-                    string_eval = "simple_config."+this_store.fields.items[i].name+" = adv_config."+this_store.fields.items[i].name+" = new Array();"+
-                    "for(var a = 0; a "+"<"+" record[i].data."+this_store.fields.items[i].name+".length; a++){"+
-                        "simple_config."+this_store.fields.items[i].name+"[a] = adv_config."+this_store.fields.items[i].name+"[a] = ["+
-                          "record[i].data."+this_store.fields.items[i].name+"[a].id, record[i].data."+this_store.fields.items[i].name+"[a].value"+
-                        "];"+
-                     "}"
-                    eval(string_eval);  
-                  }
-                }
-                this.getPanel().getComponent("ADST_panel").getComponent('query_container').show();
-                this.getPanel().getComponent("ADST_panel").getComponent("query_container").getComponent("ADSTFormPanel").add(new Talho.Rollcall.SimpleADSTContainer(simple_config));
-                this.getPanel().getComponent("ADST_panel").getComponent("query_container").getComponent("ADSTFormPanel").add(new Talho.Rollcall.AdvancedADSTContainer(adv_config));
-                this.getPanel().getComponent("ADST_panel").getComponent("query_container").getComponent("ADSTFormPanel").doLayout();
-                this.getPanel().getComponent("ADST_panel").doLayout();
-              }
-            }
-          });
-
-        }
-      },
       defaults: {
         collapsible: true,
-        split:       true,     
+        split:       true,
         cmargins:    '5 5 0 0',
         margins:     '5 0 0 0'
       },
@@ -193,7 +224,7 @@ Talho.RollcallQuery = Ext.extend(Ext.util.Observable, {
         autoScroll:  true,
         listeners: {
           render: function(this_comp){
-            new Ext.LoadMask(this_comp.getEl(), {msg:"Please wait...", store: Talho.Rollcall.init_store});  
+            new Ext.LoadMask(this_comp.getEl(), {msg:"Please wait...", store: Talho.Rollcall.init_store});
           }
         },
         items:[{
@@ -201,53 +232,22 @@ Talho.RollcallQuery = Ext.extend(Ext.util.Observable, {
           itemId: 'query_container',
           layout: 'column',
           hidden: true,
-          items:[{
-            xtype: 'form',
-            itemId: 'ADSTFormPanel',
-            columnWidth: 1,
-            labelAlign: 'top',
-            id: "ADSTFormPanel",
-            url:'/rollcall/adst',
-            buttonAlign: 'left',
-            buttons: [{
-              text: "Submit",
-              scope: this,
-              handler: function(buttonEl, eventObj){
-                Ext.getCmp('ADSTFormPanel').getForm().submit({
-                  scope: this,
-                  waitMsg: "Please wait...",
-                  waitTitle: "Loading",
-                  success: function(form, action)
-                  {
-                    Ext.getCmp('ADSTResultPanel').show();
-                    Ext.getCmp('ADSTResultPanel').processQuery(action.result);
-                  },
-                  failure: function(form, action)
-                  {
-
-                  }
-                });
-              },
-              formBind: true
-            },{
-              text: "Cancel",
-              handler: this.clearForm
-            }]
-          }]
+          listeners: {
+            enable: function() {
+              alert("Hey!");
+            }
+          },
+          items:[new Talho.Rollcall.ADSTFormPanel({})]
         },
           new Talho.Rollcall.ADSTResultPanel({})
         ]
       }]
     });
-
-    this.getPanel = function()
-    {
-      return panel;
-    }
+    Talho.Rollcall.ADST.superclass.constructor.call(this, config);
   },
   buildReportPanel: function()
   {
-    return true
+    return true;
   },
   submitQuery: function(b, e)
   {
@@ -263,10 +263,9 @@ Talho.RollcallQuery = Ext.extend(Ext.util.Observable, {
   }
 });
 
-Talho.RollcallQuery.initialize = function(config)
+Talho.Rollcall.ADST.initialize = function(config)
 {
-  var query = new Talho.RollcallQuery(config);
-  return query.getPanel();
+  return new Talho.Rollcall.ADST(config);
 }
 
-Talho.ScriptManager.reg('Talho.RollcallQuery', Talho.RollcallQuery, Talho.RollcallQuery.initialize);
+Talho.ScriptManager.reg('Talho.Rollcall.ADST', Talho.Rollcall.ADST, Talho.Rollcall.ADST.initialize);

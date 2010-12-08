1b6948c4959ae91553a6e329d52e55d069383d24
Ethan Waldo <ewaldo@talho.org>
2010-12-08 15:09:45 -0600

    Removing migration because it was already merged in to master.  Also fixed the seeds to use tea_id instead of display_name

--- db/fixtures/schools.rb
+++ db/fixtures/schools.rb
@@ -1,13 +1,13 @@
 require 'fastercsv'
 
-def build_rrd(tea_id)
+def build_rrd(identifier)
   rrd_path = Dir.pwd << "/rrd/"
 
   rrd_tool = if File.exist?(doc_yml = RAILS_ROOT+"/config/rrdtool.yml")
     YAML.load(IO.read(doc_yml))[Rails.env]["rrdtool_path"] + "/rrdtool"
   end
 
-  RRD.create("#{rrd_path}#{tea_id}_absenteeism.rrd",
+  RRD.create("#{rrd_path}#{identifier}_absenteeism.rrd",
     {
       :step  => 24.hours.seconds,
       :start => Time.local(2010,"nov",1,0,0).to_i,
@@ -23,7 +23,7 @@ def build_rrd(tea_id)
       },{
         :type => "LAST", :xff => 0.5, :steps => 1, :rows => 366
       }]
-    } , "#{rrd_tool}") unless File.exists?("#{rrd_path}#{tea_id}_absenteeism.rrd")
+    } , "#{rrd_tool}") unless File.exists?("#{rrd_path}#{identifier}_absenteeism.rrd")
   return true
 end
 

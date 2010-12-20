ff6287e07e2e589490e6ababdf7db9dd6cbfc131
Ethan Waldo <ewaldo@talho.org>
2010-12-20 09:20:14 -0600

    Fixed issues caused by renaming of rollcall classes

--- lib/models/jurisdiction.rb
+++ lib/models/jurisdiction.rb
@@ -3,7 +3,7 @@ require 'dispatcher'
 module Rollcall
   Dispatcher.to_prepare do
     ::Jurisdiction.class_eval do
-      has_many :school_districts, :include => :schools
+      has_many :school_districts, :include => :schools, :class_name => "Rollcall::SchoolDistrict"
     end
   end
 end

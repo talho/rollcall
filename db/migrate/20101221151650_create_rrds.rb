27965575deb72670a9652c447b3b871cb9ba3728
Eddie Gomez <eddieeduardo@eddiegomez.local>
2010-12-22 14:43:04 -0600

    Removing primary key to false.

--- db/migrate/20101221151650_create_rrds.rb
+++ db/migrate/20101221151650_create_rrds.rb
@@ -1,6 +1,6 @@
 class CreateRrds < ActiveRecord::Migration
   def self.up
-    create_table :rollcall_rrds, :id => false do |t|
+    create_table :rollcall_rrds do |t|
       t.integer :saved_query_id
       t.string  :file_name
       t.timestamps

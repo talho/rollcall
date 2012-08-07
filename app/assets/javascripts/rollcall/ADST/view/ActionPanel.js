9fda8d9da5690f5fdc42957bb19b6b164e8efbce
colinphanna <github@colinphanna.com>
2012-08-07 09:42:01 -0500

    added action panel

---
+++ app/assets/javascripts/rollcall/ADST/view/ActionPanel.js
@@ -0,0 +1,33 @@
+Ext.namespace("Talho.Rollcall.ADST.view");
+
+//TODO fix type
+Talho.Rollcall.ADST.view.ActionPanel = Ext.extend(Ext.Panel, {
+  layout: 'auto',
+  hidden: false,
+  
+  constructor: function (config) {
+    this.addEvents('reset', 'submitquery');        
+    this.enableBubble('submitquery');
+    this.enableBubble('reset');
+    
+    
+    Talho.Rollcall.ADST.view.ActionPanel.superclass.constructor.apply(this, config);
+  },
+  
+  initComponent: function (config) {        
+    this.buttons = [
+      //TODO move handlers up to controller
+      {text: "Submit", itemId: 'submit_ext4', scope: this, handler: function () { this.fireEvent('submitquery'); }, formBind: true},
+      {text: "Reset Form", scope: this, handler: function () { this.fireEvent('reset'); }},
+      {text: "Export Result Set", hidden: true, scope: this, handler: this._exportResultSet},
+      {text: "Create Alarm from Result Set", hidden: true, scope: this, handler: this.saveResultSet},
+      {text: "Generate Report from Result Set", hidden: true, scope: this,
+        handler: function (buttonObj, eventObj) {
+          this._showReportMenu(buttonObj.getEl(), null);
+        }
+      }
+    ];        
+      
+    Talho.Rollcall.ADST.view.ActionPanel.superclass.initComponent.apply(this, config);
+  }
+});

87bf32656180663efda8873f979b4fba87622432
colinphanna <github@colinphanna.com>
2012-10-16 14:35:25 -0500

    Feature done, Steps stubbed out

---
+++ features/step_definitions/status_steps.rb
@@ -0,0 +1,19 @@
+Given /^I have rollcall users in a variety of roles$/ do
+  
+end
+
+Given /^I have schools with reported rollcall data$/ do
+  
+end
+
+When /^I am logged in as rollcall (user|admin|superadmin)$/ do |privelege|
+  
+end
+
+Then /^I do (not)? see the status link$/ do |visible|
+  
+end
+
+Then /^I receive an emailed status report$/ do
+  
+end

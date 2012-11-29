951e89da90fe69aa4fa52e8e4c214459c348d4ed
cdubose <cdubose@marlin.(none)>
2012-11-29 10:56:33 -0600

    updates

--- app/models/rollcall/ili_report.rb
+++ app/models/rollcall/ili_report.rb
@@ -1,3 +1,18 @@
-class Rollcall::ILIReport
+class Rollcall::ILIReport < ::Report
+  view = 'rollcall/ili_report'
   
+  def self.build_report(user_id)
+    # in here we're going to put together the parameters that we want to save off for the report.
+    r = self.new user_id: user_id
+    user = ::User.find(user_id) 
+    
+    params = {school_districts: []}
+    
+    school_districts = user.school_districts
+    school_districts.each do |sd|
+      # find the absence rate for the school district
+      # find confirmed illness
+      # find 
+    end
+  end
 end

db9dc396609de9e9fc99c1dc6bd2c4a22de39027
cdubose <cdubose@marlin.(none)>
2013-01-17 08:26:47 -0600

    adding symptom tagging into the system. pre-populating the seed file with a few basic tags

--- app/models/rollcall/symptom.rb
+++ app/models/rollcall/symptom.rb
@@ -7,5 +7,6 @@
 # name,       :string(255)
 
 class Rollcall::Symptom < ActiveRecord::Base
-  self.table_name = "rollcall_symptoms" 
+  self.table_name = "rollcall_symptoms"
+  has_many :symptom_tags, :class_name => "Rollcall::SymptomTag"
 end
--- db/fixtures/symptoms.rb
+++ db/fixtures/symptoms.rb
@@ -2,13 +2,9 @@ require 'csv'
 #run through the symptoms file
 CSV.open(File.dirname(__FILE__) + '/symptoms.csv', :headers => true) do |symptoms|
   symptoms.each do |row|
-    #if icd9_code is blank, skip record
-    if row["icd9_code"].blank?
-      puts "Could not create a symptoms for #{row["icd9_code"]}; incomplete information"
-      next
-    end
-    puts "seeding #{row["name"]}" unless row["icd9_code"].blank? || Rollcall::Symptom.find_by_icd9_code(row["icd9_code"].strip)
-    #create symptom
-    symptoms = Rollcall::Symptom.find_or_create_by_icd9_code(:icd9_code => row["icd9_code"], :name => row["name"].strip)
+    symptom = Rollcall::Symptom.where(:icd9_code => row["icd9_code"], :name => row["name"]).first_or_create
+    row["matches"].split(';').each do |match|
+      Rollcall::SymptomTag.where(symptom_id: symptom.id, match: match).first_or_create
+    end if row["matches"]
   end
 end

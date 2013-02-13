0b8210a3b810602837230512450c9351bd3dfe13
colinphanna <github@colinphanna.com>
2013-02-13 08:45:05 -0600

    Maps Sufficiently Fiddled with

--- app/assets/javascripts/rollcall/map/view/Layout.js
+++ app/assets/javascripts/rollcall/map/view/Layout.js
@@ -5,51 +5,66 @@ Talho.Rollcall.Map.view.Layout = Ext.extend(Ext.Panel, {
   closable: true,
   layout: 'fit',
   border: false,
-  defaults: { border: false }
-,  title: "Rollcall Mapping", 
+  defaults: { border: false },
+  title: "Rollcall Mapping",
   
-  initComponent: function () {    
-    var store = new Ext.data.JsonStore({
-      url: 'rollcall/map',
-      method: 'GET',
-      root: 'results',      
-      idProperty: 'school_id',
-      autoLoad: false,      
-      fields: ['school_id', 'weight', 'gmap_lat', 'gmap_lng', 'display_name']
-    });
-    
-    this.store = store;
-    
+  initComponent: function () {  
     this.gmap = new Ext.ux.GMapPanel({zoomLevel: 6, region: 'center'});
-    this.start = new Ext.form.Label();
-    this.end = new Ext.form.Label();
+    this.start = new Ext.form.Label({text: '2013-01-31'});
+    this.end = new Ext.form.Label({text: '2013-01-31'});    
+    this.date_label = new Ext.form.Label({text: 'Displaying School Districts on current date'});
+    this.slider = new Ext.slider.MultiSlider({minValue: 0, values: [0], width: 250});
+    this.displaying = new Ext.form.Label({});
+    this.control_panel = new Ext.Panel({region: 'south', height: 100, items: [
+      {xtype: 'panel', layout: 'hbox', border: false, layoutConfig: { padding:'5', pack:'center', align:'middle' }, items: [
+        {xtype: 'button', text: '<<', scope: this, handler: function () { this._setDate(this.date_index - 1); }},            
+        {xtype: 'spacer', width: 20},
+        this.start,
+        {xtype: 'spacer', width: 20},
+        this.slider,
+        {xtype: 'spacer', width: 20},
+        this.end,
+        {xtype: 'spacer', width: 20},
+        {xtype: 'button', text: '>>', scope: this, handler: function () { this._setDate(this.date_index + 1); }}
+      ]},
+      {xtype: 'panel', layout: 'hbox', border: false, layoutConfig: { padding:'5', pack:'center', align:'middle' }, items: [ this.date_label ]}      
+    ]});
     
     this.items = [{xtype: 'panel', layout: 'border', items:[
         this.gmap,
-        {xtype: 'panel', region: 'south', height: 100, items: [
-          {xtype: 'panel', layout: 'hbox', border: false, items: [
-            {xtype: 'button', text: '<<'},
-            {xtype: 'spacer', width: 20},
-            {xtype: 'label', text: 'start'},
-            {xtype: 'spacer', width: 20},
-            new Ext.slider.MultiSlider({minValue: 0, maxValue: 7, values: [0], width: 250}),
-            {xtype: 'spacer', width: 20},
-            {xtype: 'label', text: 'end'},
-            {xtype: 'spacer', width: 20},
-            {xtype: 'button', text: '>>'}
-          ]},
-          {xtype: 'button', text: 'Play'}
-        ]}
+        this.control_panel
       ]
     }];
       
-    this.on('afterrender', this._loadStore, this);
+    this.date_mode = "sd";
+      
+    Ext.Ajax.request({
+      url: '/rollcall/map',
+      method: 'GET',
+      scope: this,
+      params: {school_district: true},
+      success: function (response) {
+        var data = Ext.decode(response.responseText);
+        this._loadData(data);                 
+      }      
+    });
           
     Talho.Rollcall.Map.view.Layout.superclass.initComponent.call(this);
   },
   
-  _loadStore: function () {
-    this.store.load({callback: this._mapsCheck, scope: this});
+  _loadData: function (data) {
+    this.start.setText(data.start);
+    this.end.setText(data.end);
+    this.data = data.results;
+    this.date = this.data[0].date;
+    this.date_label.setText('Displaying ' + (this.data_mode == "sd" ? 'School Districts' : 'Schools') + ' on ' + this.date);
+    this.date_index = 0;
+    this.slider.setMaxValue(this.data.length);
+    this.slider.addListener('change', function (slider, newValue, thumb) {
+      this._setDate(newValue);
+    }, this)
+
+    this._mapsCheck();
   },
   
   _mapsCheck: function () {
@@ -65,23 +80,105 @@ Talho.Rollcall.Map.view.Layout = Ext.extend(Ext.Panel, {
     var latlngbounds = new google.maps.LatLngBounds();
     var data = new google.maps.MVCArray();
     
-    this.store.each(function (record) {
-      var loc = new google.maps.LatLng(record.get("gmap_lat"), record.get("gmap_lng"));
-      data.push({
-        location: loc,
-        weight: parseInt(record.get('weight')) 
-      });
-      latlngbounds.extend(loc);
+    Ext.each(this.data, function (record) {
+      if (record.date == this.date) {
+        Ext.each(record.schools, function (school) {
+          var loc = new google.maps.LatLng(school.gmap_lat, school.gmap_lng);
+          data.push({
+            location: loc,
+            weight: parseInt(school.weight) 
+          });
+          latlngbounds.extend(loc);
+        });
+      }
     });    
     
-    if (!latlngbounds.isEmpty()) {
+    if (!latlngbounds.isEmpty() && !this.centered) {
       this.gmap.gmap.fitBounds(latlngbounds);
+      this.centered = true;
     }
     
-    var heatmap = new google.maps.visualization.HeatmapLayer({
-      data: data,
-      map: this.gmap.gmap,
-      opacity: .6
-    });          
+    var gmap = this.gmap.gmap;
+    
+    if (!this.heatmap) {
+      this.heatmap = new google.maps.visualization.HeatmapLayer({      
+        map: this.gmap.gmap,
+        opacity: .6,
+        radius: this._getRadius(gmap),
+        maxIntensity: 100
+      });
+    }
+    
+    var heatmap = this.heatmap;
+    var getRadius = this._getRadius;
+    
+    google.maps.event.addListener(gmap, 'zoom_changed', function() {
+      if (gmap.getZoom() > 8) {
+        Ext.getCmp("mapping")._setDataMode("s");
+        heatmap.setOptions({radius: getRadius(gmap)});
+      }
+      else {
+        Ext.getCmp("mapping")._setDataMode("sd");
+        heatmap.setOptions({radius: getRadius(gmap)});
+      }
+    });
+    
+    this.heatmap.setData(data);    
+    this.heatmap.setMap(this.gmap.gmap);
+  },
+  
+  _setDate: function (index) {
+    if (index < 0) {
+     index = this.data.length - 1;     
+    }
+    else if (index > this.data.length - 1) {
+      index = 0;
+    }
+    
+    this.date_index = parseInt(index);
+    this.slider.setValue(0, this.date_index); 
+    this.date = this.data[this.date_index].date;
+    
+    this.date_label.setText('Displaying ' + (this.data_mode == "sd" ? 'School Districts' : 'Schools') + ' on ' + this.date);         
+    
+    this._renderGmapOverlays();    
+  },
+  
+  _getRadius: function (gmap) {
+    var radii = [2,2,3,3,4,6,8,10,12,5,5,5,6,6,10,10,10,10,10,10,10];
+    
+    return radii[gmap.getZoom()];
+  },
+  
+  _setDataMode: function (mode) {
+    if (mode != this.data_mode) {
+      this.data_mode = mode;
+      this.heatmap.setMap(null);
+      if (this.data_mode == "sd") {
+        this.displaying.setText("Displaying: School Districts");
+        Ext.Ajax.request({
+          url: '/rollcall/map',
+          method: 'GET',
+          scope: this,
+          params: {school_district: true},
+          success: function (response) {
+            var data = Ext.decode(response.responseText);
+            this._loadData(data);                 
+          }      
+        });
+      }
+      else {
+        this.displaying.setText("Displaying: Schools");
+        Ext.Ajax.request({
+          url: '/rollcall/map',
+          method: 'GET',
+          scope: this,          
+          success: function (response) {
+            var data = Ext.decode(response.responseText);
+            this._loadData(data);                 
+          }      
+        });
+      }      
+    }
   }
 });
--- app/controllers/rollcall/map_controller.rb
+++ app/controllers/rollcall/map_controller.rb
@@ -1,42 +1,70 @@
-
 class Rollcall::MapController < Rollcall::RollcallAppController
   respond_to :json
   layout false
-  
   def index
-    @start = 7.days.ago.to_date
-    @end = DateTime.now.to_date
-    @results = Array.new
-    
-    flat_results = Rollcall::School
-      .joins("inner join rollcall_school_daily_infos i on i.school_id = rollcall_schools.id")
-      .where("report_date between ? and ?", @start, @end)
+    @start = 8.days.ago.to_date
+    @end = 1.days.ago.to_date
+
+    if params[:school_district].present?
+      @results = get_by_school_district 
+    else
+      @results = get_by_school
+    end
+
+    respond_with(@results, @start, @end)
+  end
+
+  private
+  
+  def get_by_school_district
+    flat_results = Rollcall::SchoolDistrict
+      .joins("inner join (select report_date, district_id, sum(total_absent) as total_absent, sum(total_enrolled) as total_enrolled from rollcall_school_daily_infos inner join rollcall_schools on rollcall_schools.id = school_id group by report_date, district_id) as totals on totals.district_id = rollcall_school_districts.id")
+      .joins("inner join (select district_id, (atan2(avg(cos(gmap_lat * pi() / 180) * sin(gmap_lng * pi() / 180)), avg(cos(gmap_lat * pi() / 180) * cos(gmap_lng * pi() / 180)))) * 180 / pi() as gmap_lng, (atan2(avg(sin(gmap_lat * pi() / 180)), sqrt(avg(cos(gmap_lat * pi() / 180) * cos(gmap_lng * pi() / 180)) ^ 2 + avg(cos(gmap_lat * pi() / 180) * sin(gmap_lng * pi() / 180)) ^ 2))) * 180 / pi() as gmap_lat from rollcall_schools where district_id is not null group by district_id) as sdc on sdc.district_id = rollcall_school_districts.id")
+      .where("report_date >= ? and report_date <= ?", @start, @end)
       .where("total_enrolled <> 0")
-      .where("gmap_lat is not null and gmap_lng is not null")
-      .select("display_name, gmap_lat, gmap_lng, rollcall_schools.id, report_date")      
-      .select("round(Cast(total_absent as float) / Cast(total_enrolled as float) * 100) as weight")
-      .order("report_date")
+      .select("name as display_name, sdc.gmap_lat, sdc.gmap_lng, rollcall_school_districts.id, report_date")
+      .select("round(Cast(total_absent as float) / Cast(total_enrolled as float) * 100) as weight")      
+      .order("report_date, rollcall_school_districts.id")
       .all
       
+    process_query flat_results
+  end
+
+  def get_by_school
+    flat_results = Rollcall::School
+      .joins("inner join rollcall_school_daily_infos i on i.school_id = rollcall_schools.id")
+      .where("report_date >= ? and report_date <= ?", @start, @end)
+      .where("total_enrolled <> 0")
+      .where("gmap_lat is not null and gmap_lng is not null")
+      .select("display_name, gmap_lat, gmap_lng, rollcall_schools.id, report_date")
+      .select("round(Cast(total_absent as float) / Cast(total_enrolled as float) * 100) as weight")
+      .order("report_date, rollcall_schools.id")
+      .all    
+
+    process_query flat_results
+  end
+  
+  def process_query(flat_results)
     iterator_date = flat_results[0][:report_date]
     schools = Array.new
-    
+    results = Array.new
+  
     flat_results.each_with_index do |s, i|
-      
-      if flat_results.count == i - 1
+
+      if flat_results.count - 1 == i
         schools.push(s)
-        @results.push({record_date: iterator_date, schools: schools})
+        results.push({record_date: iterator_date, schools: schools})
       end
-      
+
       if iterator_date != s.report_date
-        @results.push({record_date: iterator_date, schools: schools})
+        results.push({record_date: iterator_date, schools: schools})
         schools = Array.new
         iterator_date = s.report_date
       else
         schools.push(s)
-      end            
+      end
     end
     
-    respond_with(@results, @start, @end)
+    return results
   end
 end

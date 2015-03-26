983180e4755059ea2aeed243f7cca304e92dc2c0
Charles DuBose <kayakyakr@gmail.com>
2015-03-26 15:46:55 -0500

    adding deploy setup actions

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,21 +3,62 @@ lock '3.4.0'
 
 set :application, 'rollcall'
 set :repo_url, 'git@github.com:talho/rollcall.git'
+set :branch, "master"
+set :scm, :git
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, "/home/ubuntu/#{fetch(:application)}"
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, %w{config/application.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle}
 
 # Default value for keep_releases is 5
 set :keep_releases, 5
 
+set :rvm1_ruby_version, "ruby-2.2.0@rollcall"
+
 namespace :deploy do
+  task :setup do
+    on roles :app do
+      execute :'mkdir', '-p', "#{shared_path}/config"
+    end
+  end
+
+  before :setup, 'deploy:check:make_linked_dirs'
+
+  before :setup, :create_framework do
+    on roles :app do
+      upload! 'config/application.yml', shared_path.join('config/application.yml')
+    end
+  end
+
+  after :setup, :install_prereqs do
+    on roles :app do
+      execute :sudo, :"apt-get", :install, :'nodejs', :'git', :'libpq-dev', "-y"
+    end
+  end
+
+  desc 'Seed'
+  after :finishing, :seed do
+    on roles :db do
+      within current_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'db:seed'
+        end
+      end
+    end
+  end
+
+  before :'rvm1:install:rvm', :install_gpg_key do
+    on roles :app do
+      execute :gpg, '--keyserver hkp://keys.gnupg.net', '--recv-keys D39DC0E3'
+    end
+  end
+  after :setup, 'rvm1:install:rvm'
+  after :setup, 'rvm1:install:ruby'
+
 end
